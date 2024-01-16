101c73f8c2e11f99907f8a35f369878535d5ef9b
raghuprasadks <prasadraghuks@gmail.com>
2024-01-16 15:58:08 +0530

    updated

--- programs/javabasics/src/collectionsdemo/Account.java
+++ programs/javabasics/src/collectionsdemo/Account.java
@@ -2,6 +2,38 @@ package collectionsdemo;
 
 public class Account {
 	private int actnum;
+	public int getActnum() {
+		return actnum;
+	}
+
+	public void setActnum(int actnum) {
+		this.actnum = actnum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
 	private String name;
 	private String bank;
 	private int balance;
