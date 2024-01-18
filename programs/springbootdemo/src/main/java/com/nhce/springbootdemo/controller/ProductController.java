b82e7c25931f9d463db2dc322866a037275ac023
raghuprasadks <prasadraghuks@gmail.com>
2024-01-18 16:20:58 +0530

    spring boot

--- programs/springbootdemo/src/main/java/com/nhce/springbootdemo/controller/ProductController.java
+++ programs/springbootdemo/src/main/java/com/nhce/springbootdemo/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.nhce.springbootdemo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +33,24 @@ public class ProductController {
 		return this.productList;		
 	}
 
+	@DeleteMapping("/deleteProduct/{code}")
+	public String deleteProduct(@PathVariable int code) {
+		String msg="product not found";
+		Product prodtodelete=null;
+		boolean isFound = false;
+		for(Product prod : this.productList) {
+			if (prod.getCode() ==code) {
+				prodtodelete=prod;
+				isFound=true;			
+				break;
+			}
+		}
+		if(isFound) {
+			this.productList.remove(prodtodelete);
+			msg="product with code "+code +" deleted successfully ";
+		}
+		return msg;		
+	 
+			
+	}
 }
