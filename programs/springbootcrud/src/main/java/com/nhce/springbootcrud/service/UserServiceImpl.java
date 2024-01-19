243f9ca53c2fb837fb19011dce6e89e7c3369519
raghuprasadks <prasadraghuks@gmail.com>
2024-01-19 11:37:08 +0530

    jpa demo

--- programs/springbootcrud/src/main/java/com/nhce/springbootcrud/service/UserServiceImpl.java
+++ programs/springbootcrud/src/main/java/com/nhce/springbootcrud/service/UserServiceImpl.java
@@ -1,39 +1,38 @@
 package com.nhce.springbootcrud.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nhce.springbootcrud.dao.UserRepository;
 import com.nhce.springbootcrud.entity.User;
-
 @Service
 public class UserServiceImpl implements UserService{
-
+	@Autowired
 	private UserRepository userRepo;
-	
 	@Override
 	public void save(User user) {
-		// TODO Auto-generated method stub
-		
+		userRepo.save(user);		
 	}
-
 	@Override
 	public List<User> getUser() {
 		// TODO Auto-generated method stub
-		return null;
+		return userRepo.findAll();
 	}
-
 	@Override
 	public User findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<User> userResult = userRepo.findById(id);
+		User user = null;
+		if (userResult.isPresent()) {
+			user = userResult.get();
+		} 
+		return user;
 	}
-
 	@Override
 	public void Delete(Integer id) {
 		// TODO Auto-generated method stub
-		
+		userRepo.deleteById(id);		
 	}
-
 }
